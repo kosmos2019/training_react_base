8429d331cf33e8504f300a0d4a209ef96bd1e6c4
jawad.byad@outlook.fr <AL1078406@admin.hubtotal.net>
2023-11-13 13:08:24 +0100

    ReactRef: new goal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import GoalList from './components/GoalList';
+import { useState } from 'react';
+
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
 import './App.css';
 
 function App() {
+  const [courseGoals, setCourseGoals] = useState([
+    {id: 'c1', text: 'Finish the course'},
+    {id: 'c2', text: 'Learn about the Course Main Topic'},
+    {id: 'c3', text: 'Help other students in the Course QA'},
+  ]);
+
+  const addNewGoalHandler = newGoal => {
+    // courseGoals.push(newGoal);
+    // console.log(courseGoals);
+    //setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
+  }
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
